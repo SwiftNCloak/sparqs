7299c1be5a2ad76371d103d276ede0117c71eeb7
SwiftNCloak <markyjamesbonifacio@gmail.com>
2024-07-10 22:37:48 +0800

    started with the basics

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,7 +1,13 @@
 export default function NavBar() {
     return (
-      <div className="w-full px-6 py-4 bg-themeOrange-100 text-textWhite">
-        <h2 className="text-2xl font-extrabold">SparQs</h2>
+      <div className="w-full px-6 py-3 bg-themeOrange-100 text-textWhite items-center 
+                      justify-between flex">
+        <div className="items-center justify-between flex">
+            <h2 className="text-2xl font-extrabold">SparQs</h2>
+        </div>
+        <div className="items-center justify-between flex">
+            image
+        </div>
       </div>
     );
   }
