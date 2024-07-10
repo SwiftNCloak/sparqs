57f16bbd0b524c8d69e105cedc3a408b716de726
SwiftNCloak <markyjamesbonifacio@gmail.com>
2024-07-10 23:41:07 +0800

    fine

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -1,9 +1,9 @@
 export default function SideBar() {
     return (
       // The sidebar
-      <div className="border-r border-themeWhite-200 w-56 top-[57px] bottom-0 left-0 px-6 py-3 bg-themeWhite-100 box-border">
-        <h2 className="font-medium">Home</h2>
-        <h2 className="font-medium">Calendar</h2>
+      <div className="border-r border-themeWhite-200 py-3 bg-themeWhite-100 box-border">
+        <h2 className="px-6 py-1 font-medium">Home</h2>
+        <h2 className="px-6 py-1 font-medium">Calendar</h2>
       </div>
     );
   }
