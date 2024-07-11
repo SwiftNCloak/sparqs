579d335d0ad1ab93c616430f6e0b22cde4ba6312
SwiftNCloak <markyjamesbonifacio@gmail.com>
2024-07-11 16:58:03 +0800

    routing works

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      IT WORKS!!!
+      HOMEPAGE
     </div>
   );
 }
--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function SideBar() {
     return (
       // The sidebar
       <div className="border-r border-themeWhite-200 py-3 bg-themeWhite-100 box-border w-52">
-        <div className="px-6 py-2 font-medium no-underline hover:bg-themeOrange-200 hover:text-themeWhite-100">Home</div>
-        <div className="px-6 py-2 font-medium no-underline hover:bg-themeOrange-200 hover:text-themeWhite-100">Calendar</div>
+        <Link href='/' className="px-6 py-2 font-medium no-underline hover:bg-themeOrange-200 hover:text-themeWhite-100">Home</Link>
+        <Link href='/calendar' className="px-6 py-2 font-medium no-underline hover:bg-themeOrange-200 hover:text-themeWhite-100">Calendar</Link>
       </div>
     );
   }
